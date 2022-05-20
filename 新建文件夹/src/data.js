47f408a4d9710bbc8b51c79e021b7996d368d0ef
ShadowDawnme <92978479+ShadowDawnme@users.noreply.github.com>
2022-05-20 22:12:29 +0800

    Add files via upload

---
+++ 新建文件夹/src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://cdn.pixabay.com/photo/2022/02/12/18/19/monkey-7009603_960_720.jpg",
+      title: "Monkeys",
+      desc: "CARTOON APE CUSTOMIZED WITH UNIQUE COLOR SCHEMES.",
+      bg: "D3D3D3",
+    },
+    {
+      id: 2,
+      img: "https://cdn.pixabay.com/photo/2022/02/19/15/04/cat-7022877_960_720.jpg",
+      title: "CATS",
+      desc: "CARTOON APE CUSTOMIZED WITH UNIQUE COLOR SCHEMES.",
+      bg: "D3D3D3",
+    },
+    {
+      id: 3,
+      img: "https://cdn.pixabay.com/photo/2022/03/01/02/51/galaxy-7040416_960_720.png",
+      title: "OTHERS",
+      desc: "EXPLORE MORE",
+      bg: "D3D3D3",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://cdn.pixabay.com/photo/2022/02/12/18/12/monkey-7009595_960_720.jpg",
+      cat: ""
+    },
+    {
+      id: 2,
+      img: "https://cdn.pixabay.com/photo/2022/02/19/15/05/dark-7022879_960_720.jpg",
+      cat: ""
+    },
+    {
+      id: 3,
+      img: "https://cdn.pixabay.com/photo/2021/12/06/10/32/non-fungible-token-6849853_960_720.jpg",
+      cat: ""
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id: "625538963e728d9f94675eb9",
+      img:"https://cdn.pixabay.com/photo/2022/02/14/02/52/monkey-7012380__340.png",
+    },
+    {
+      id: "62556e2f88a446749bc118b0",
+      img:"https://cdn.pixabay.com/photo/2022/02/19/15/04/cat-7022875_960_720.png",
+    },
+    {
+      id: "625c211f83a32d610d5b6f6d",
+      img:"https://cdn.pixabay.com/photo/2022/02/19/15/04/cat-7022874_960_720.png",
+    },
+    {
+      id: "625c213483a32d610d5b6f70",
+      img:"https://cdn.pixabay.com/photo/2022/02/08/06/35/bear-7000852_960_720.jpg",
+    },
+    {
+      id: "625c214083a32d610d5b6f73",
+      img:"https://cdn.pixabay.com/photo/2022/01/30/13/05/earth-6980848_960_720.jpg",
+    },
+    {
+      id: "625c215783a32d610d5b6f76",
+      img:"https://cdn.pixabay.com/photo/2022/03/03/20/47/the-simpson-7046041_960_720.jpg",
+    },
+    {
+      id: "625c21e983a32d610d5b6f7c",
+      img:"https://cdn.pixabay.com/photo/2022/02/19/18/56/rosary-7023271_960_720.jpg",
+    },
+    {
+      id: "625c220383a32d610d5b6f7f",
+      img:"https://cdn.pixabay.com/photo/2022/02/19/15/03/cat-7022873_960_720.png",
+    },
+  ]
